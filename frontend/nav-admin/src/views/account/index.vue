912d8c4b3c659922b1f4218592b80a5ca2dbe29e
diyun <1218967186@qq.com>
2024-04-10 21:04:50 +0800

    提交一个列表

--- frontend/nav-admin/src/views/account/index.vue
+++ frontend/nav-admin/src/views/account/index.vue
@@ -3,9 +3,26 @@ defineOptions({
   // name 作为一种规范最好必须写上并且和路由的name保持一致
   name: "Account"
 });
+import { tableData } from "./data";
+
+const columns: TableColumnList = [
+  {
+    label: "日期",
+    prop: "date"
+  },
+  {
+    label: "姓名",
+    prop: "name"
+  },
+  {
+    label: "地址",
+    prop: "address"
+  }
+];
+
 </script>
 
 <template>
-  <h1>修改密码</h1>
+  <pure-table :data="tableData" :columns="columns" />
 </template>
 
