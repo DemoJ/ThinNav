f3501b4b7f86f67af04b03c6867f9ca876824982
狄云 <dy@huashixinli.com>
2024-08-13 14:50:11 +0800

    增加网站描述的提示框

--- frontend/navigation/src/components/WebsiteCard.vue
+++ frontend/navigation/src/components/WebsiteCard.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="website-card" @click="navigateToUrl(link.url)">
+  <div class="website-card" @click="navigateToUrl(link.url)" @mouseover="showTooltip" @mouseleave="hideTooltip">
     <div class="website-icon">
       <img :src="link.icon_url" alt="icon" />
     </div>
     <div class="website-info">
       <p class="website-name">{{ link.name }}</p>
       <p class="website-description">{{ link.description }}</p>
+    </div>
+    <div v-if="tooltipVisible" class="tooltip">
+      {{ link.description }}
     </div>
   </div>
 </template>
@@ -16,9 +19,20 @@ export default {
   props: {
     link: Object
   },
+  data() {
+    return {
+      tooltipVisible: false
+    };
+  },
   methods: {
     navigateToUrl(url) {
       window.open(url, '_blank');
+    },
+    showTooltip() {
+      this.tooltipVisible = true;
+    },
+    hideTooltip() {
+      this.tooltipVisible = false;
     }
   }
 };
@@ -31,9 +45,10 @@ export default {
   opacity: 1;
   border-radius: 8px;
   background: rgba(255, 255, 255, 1);
-  box-shadow: 0px 4px 4px  rgba(240, 244, 249, 0.1);
+  box-shadow: 0px 4px 4px rgba(240, 244, 249, 0.1);
   display: flex;
   cursor: pointer; /* 当鼠标悬停时显示手指图标 */
+  position: relative; /* 为了定位提示框 */
 }
 
 .website-icon {
@@ -59,9 +74,50 @@ export default {
   padding-bottom: 6px;
 }
 
-.website-description{
+.website-description {
   margin: 0; /* 移除默认的外边距 */
   font-size: 12px; /* 描述文字大小 */
   color: rgba(153, 153, 153, 1);
+  display: -webkit-box; /* 使用弹性盒布局 */
+  -webkit-box-orient: vertical; /* 垂直方向布局 */
+  -webkit-line-clamp: 2; /* 限制显示两行 */
+  overflow: hidden; /* 超出部分隐藏 */
+  text-overflow: ellipsis; /* 超出部分用省略号代替 */
+  max-height: 3em; /* 两行文字的高度 */
+  padding-right: 44px; /* 添加右侧内边距 */
+}
+
+.tooltip {
+  position: absolute; /* 定位提示框 */
+  bottom: -60px; /* 距离卡片底部 */
+  left: 50%; /* 居中对齐 */
+  transform: translateX(-50%); /* 居中对齐 */
+  background-color: rgba(0, 0, 0, 0.8); /* 设置背景颜色为半透明黑色 */
+  color: rgba(255, 255, 255, 0.9); /* 设置提示文字颜色 */
+  padding: 8px;
+  border-radius: 4px;
+  width: 200px; /* 设置提示框的宽度 */
+  max-width: 200px; /* 设置最大宽度 */
+  white-space: normal; /* 允许自动换行 */
+  word-break: break-word; /* 单词换行 */
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2); /* 添加阴影 */
+  font-size: 12px; /* 调整提示文字大小 */
+  opacity: 0; /* 初始状态下不可见 */
+  transition: opacity 0.3s; /* 平滑过渡 */
+  z-index: 1000; /* 确保提示框在最上层 */
+}
+
+.tooltip::after {
+  content: "";
+  position: absolute;
+  bottom: 100%; /* 定位到提示框的顶部 */
+  left: 50%;
+  transform: translateX(-50%);
+  border-width: 5px;
+  border-style: solid;
+  border-color: rgba(0, 0, 0, 0.8) transparent transparent transparent; /* 三角形的颜色 */
+}
+.website-card:hover .tooltip {
+  opacity: 1; /* 鼠标悬停时显示提示框 */
 }
 </style>
