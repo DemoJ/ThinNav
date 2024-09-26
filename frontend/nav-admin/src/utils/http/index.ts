4cee03bc2409a5815b276c66b4a6e2a36e9009d2
狄云 <dy@huashixinli.com>
2024-09-26 13:09:23 +0800

    修正refreshToken白名单地址错误

--- frontend/nav-admin/src/utils/http/index.ts
+++ frontend/nav-admin/src/utils/http/index.ts
@@ -75,7 +75,7 @@ class PureHttp {
           return config;
         }
         /** 请求白名单，放置一些不需要token的接口（通过设置请求白名单，防止token过期后再请求造成的死循环问题） */
-        const whiteList = ["/api/admin/refresh-token", "/admin/login"];
+        const whiteList = ["/admin/refresh-token", "/admin/login"];
         return whiteList.find(url => url === config.url)
           ? config
           : new Promise(resolve => {
